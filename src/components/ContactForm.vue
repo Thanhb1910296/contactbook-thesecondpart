541494b0d7a708e65cb6233df3142c7959ddddb1
Thanhb1910296 <thanhb1910296@student.ctu.edu.vn>
2022-10-28 12:36:54 +0700

    Tao trang them moi mot liên he

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -35,53 +35,53 @@
     </Form>
 </template>
 <script>
-    import * as yup from "yup";
-    import { Form, Field, ErrorMessage } from "vee-validate";
-    export default {
-        components: {
-            Form,
-            Field,
-            ErrorMessage,
+import * as yup from "yup";
+import { Form, Field, ErrorMessage } from "vee-validate";
+export default {
+    components: {
+        Form,
+        Field,
+        ErrorMessage,
+    },
+    emits: ["submit:contact", "delete:contact"],
+    props: {
+        contact: { type: Object, required: true }
+    },
+    data() {
+        const contactFormSchema = yup.object().shape({
+            name: yup
+                .string()
+                .required("Tên phải có giá trị.")
+                .min(2, "Tên phải ít nhất 2 ký tự.")
+                .max(50, "Tên có nhiều nhất 50 ký tự."),
+            email: yup
+                .string()
+                .email("E-mail không đúng.")
+                .max(50, "E-mail tối đa 50 ký tự."),
+            address: yup.string().max(100, "Địa chỉ tối đa 100 ký tự."),
+            phone: yup
+                .string()
+                .matches(
+                    /((09|03|07|08|05)+([0-9]{8})\b)/g,
+                    "Số điện thoại không hợp lệ."
+                ),
+        });
+        return {
+           
+            contactLocal: this.contact,
+            contactFormSchema,
+        };
+    },
+    methods: {
+        submitContact() {
+            this.$emit("submit:contact", this.contactLocal);
         },
-        emits: ["submit:contact", "delete:contact"],
-        props: {
-            contact: { type: Object, required: true }
+        deleteContact() {
+            this.$emit("delete:contact", this.contactLocal.id);
         },
-        data() {
-            const contactFormSchema = yup.object().shape({
-                name: yup
-                    .string()
-                    .required("Tên phải có giá trị.")
-                    .min(2, "Tên phải ít nhất 2 ký tự.")
-                    .max(50, "Tên có nhiều nhất 50 ký tự."),
-                email: yup
-                    .string()
-                    .email("E-mail không đúng.")
-                    .max(50, "E-mail tối đa 50 ký tự."),
-                address: yup.string().max(100, "Địa chỉ tối đa 100 ký tự."),
-                phone: yup
-                    .string()
-                    .matches(
-                        /((09|03|07|08|05)+([0-9]{8})\b)/g,
-                        "Số điện thoại không hợp lệ."
-                    ),
-            });
-            return {
-            
-                contactLocal: this.contact,
-                contactFormSchema,
-            };
-        },
-        methods: {
-            submitContact() {
-                this.$emit("submit:contact", this.contactLocal);
-            },
-            deleteContact() {
-                this.$emit("delete:contact", this.contactLocal.id);
-            },
-        },
-    };
+    },
+};
 </script>
 <style scoped>
-    @import "@/assets/form.css";
+@import "@/assets/form.css";
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,43 @@
+// import { createWebHistory, createRouter } from "vue-router";
+// import ContactBook from "@/views/ContactBook.vue";
+// const routes = [
+//     {
+//         path: "/",
+//         name: "contactbook",
+//         component: ContactBook,
+//     },
+
+//     {
+//         path: "/contacts/create",
+//         name: "contact.add",
+//         component: () => import("@/views/ContactAdd.vue"),
+//         props: true 
+//     },
+
+//     {
+//         path: "/contacts/:id",
+//         name: "contact.edit",
+//         component: () => import("@/views/ContactEdit.vue"),
+//         props: true
+//     },
+
+
+
+//     {
+//         path: "/:pathMatch(.*)*",
+//         name: "notFound",
+//         component: () => import("@/views/NotFound.vue"),
+//     },
+// ];
+// const router = createRouter({
+//     history: createWebHistory(import.meta.env.BASE_URL),
+//     routes,
+// });
+
+// export default router;
+
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
-
 const routes = [
     {
         path: "/",
@@ -10,23 +47,29 @@ const routes = [
 
     {
         path: "/:pathMatch(.*)*",
-        name: "notfound",
+        name: "notFound",
         component: () => import("@/views/NotFound.vue"),
+        path: "/contact/create",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+        props: true,
     },
 
     {
         path: "/contacts/:id",
         name: "contact.edit",
         component: () => import("@/views/ContactEdit.vue"),
-        props: true
+        props: true,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
     },
 ];
-
-
-
 const router = createRouter({
-        history: createWebHistory(import.meta.env.BASE_URL),
-        routes,
-    });
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+});
 
 export default router;
